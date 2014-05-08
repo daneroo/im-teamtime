4a0f6a63b605e5f7f17e814009bb1fd481f0f3cf
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-05-08 02:00:10 -0400

    yo cg-angular

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('teamtime', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('teamtime').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('teamtime').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
